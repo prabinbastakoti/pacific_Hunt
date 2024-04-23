e49c933b1636c89c0dc1fa723c0f961f134891f8
Prabin Bastakoti <Prabinbastakoti1@gmail.com>
2024-04-23 16:53:26 +0545

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,41 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Homepage from "./routes/Homepage";
-import Layout from "./routes/Layout";
-import Jobspage from "./routes/Jobspage";
-import Companypage from "./routes/Companypage";
-import Login from "./routes/Login";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Homepage from './routes/homePage';
+import Layout from './routes/layout';
+import Jobspage from './routes/jobsPage';
+import Companypage from './routes/companyPage';
+import Login from './routes/login';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Homepage />,
+      },
+      {
+        path: '/Home',
+        element: <Homepage />,
+      },
+      {
+        path: '/Jobs',
+        element: <Jobspage />,
+      },
+      {
+        path: '/Companies',
+        element: <Companypage />,
+      },
+
+      {
+        path: '/About us',
+        element: <Login />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "/",
-          element: <Homepage />,
-        },
-        {
-          path: "/Home",
-          element: <Homepage />,
-        },
-        {
-          path: "/Jobs",
-          element: <Jobspage />,
-        },
-        {
-          path: "/Companies",
-          element: <Companypage />,
-        },
-
-        {
-          path: "/About us",
-          element: <Login />,
-        },
-      ],
-    },
-  ]);
-
   return <RouterProvider router={router} />;
 }
 
